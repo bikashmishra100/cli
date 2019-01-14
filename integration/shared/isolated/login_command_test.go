8ec7d61a7846119a756e9ca6322eec2e5cfae688
Magesh Kumar Murali <mmurali@pivotal.io>
2019-01-14 15:21:49 -0800

    WIP Added integration tests for `cf login` command -a flag

--- integration/shared/isolated/login_command_test.go
+++ integration/shared/isolated/login_command_test.go
@@ -1,6 +1,9 @@
 package isolated
 
 import (
+	"regexp"
+	"strings"
+
 	"code.cloudfoundry.org/cli/integration/helpers"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -16,19 +19,128 @@ var _ = Describe("login command", func() {
 		buffer.Write([]byte("\n"))
 	})
 
-	When("the API endpoint is not set", func() {
-		BeforeEach(func() {
-			helpers.UnsetAPI()
-			buffer = NewBuffer()
-			buffer.Write([]byte("\n"))
+	Describe("Help Text", func() {
+		When("--help flag is set", func() {
+			It("Displays the command usage", func() {
+				session := helpers.CF("login", "--help")
+				Eventually(session).Should(Exit(0))
+
+				Expect(session).Should(Say("NAME:\n"))
+				Expect(session).Should(Say("login - Log user in"))
+
+				Expect(session).Should(Say("USAGE:\n"))
+				Expect(session).Should(Say(`cf login \[-a API_URL\] \[-u USERNAME\] \[-p PASSWORD\] \[-o ORG\] \[-s SPACE\] \[--sso | --sso-passcode PASSCODE\]`))
+
+				Expect(session).Should(Say("WARNING:\n"))
+				Expect(session).Should(Say("Providing your password as a command line option is highly discouraged\n"))
+				Expect(session).Should(Say("Your password may be visible to others and may be recorded in your shell history\n"))
+
+				Expect(session).Should(Say("EXAMPLES:\n"))
+				Expect(session).Should(Say(regexp.QuoteMeta("cf login (omit username and password to login interactively -- cf will prompt for both)")))
+				Expect(session).Should(Say(regexp.QuoteMeta("cf login -u name@example.com -p pa55woRD (specify username and password as arguments)")))
+				Expect(session).Should(Say(regexp.QuoteMeta("cf login -u name@example.com -p \"my password\" (use quotes for passwords with a space)")))
+				Expect(session).Should(Say(regexp.QuoteMeta("cf login -u name@example.com -p \"\\\"password\\\"\" (escape quotes if used in password)")))
+				Expect(session).Should(Say(regexp.QuoteMeta("cf login --sso (cf will provide a url to obtain a one-time passcode to login)")))
+
+				Expect(session).Should(Say("ALIAS:\n"))
+				Expect(session).Should(Say("l"))
+
+				Expect(session).Should(Say("OPTIONS:\n"))
+				Expect(session).Should(Say(`-a\s+API endpoint \(e.g. https://api\.example\.com\)`))
+				Expect(session).Should(Say(`-o\s+Org`))
+				Expect(session).Should(Say(`-p\s+Password`))
+				Expect(session).Should(Say(`-s\s+Space`))
+				Expect(session).Should(Say(`--skip-ssl-validation\s+Skip verification of the API endpoint\. Not recommended\!`))
+				Expect(session).Should(Say(`--sso\s+Prompt for a one-time passcode to login`))
+				Expect(session).Should(Say(`--sso-passcode\s+One-time passcode`))
+				Expect(session).Should(Say(`-u\s+Username`))
+
+				Expect(session).Should(Say("SEE ALSO:\n"))
+				Expect(session).Should(Say("api, auth, target"))
+			})
+		})
+	})
+
+	Describe("API Endpoint", func() {
+
+		When("the API endpoint is not set", func() {
+			BeforeEach(func() {
+				helpers.UnsetAPI()
+			})
+
+			It("prompts the user for an endpoint", func() {
+				buffer = NewBuffer()
+				buffer.Write([]byte("\n"))
+				session := helpers.CFWithStdin(buffer, "login")
+				Eventually(session).Should(Say("API endpoint>"))
+				session.Interrupt()
+				Eventually(session).Should(Exit())
+			})
+
+			When("the API is set using -a flag", func() {
+				FIt("sets the api endpoing and does not ask the user for api prompt", func() {
+					apiURL := helpers.GetAPI()
+					session := helpers.CF("login", "-a", apiURL, "--skip-ssl-validation")
+					Eventually(session).Should(Say("API endpoint: %s", apiURL))
+					Consistently(session).ShouldNot(Say("API endpoint>"))
+					session.Interrupt()
+					Eventually(session).Should(Exit())
+
+					session = helpers.CF("api")
+					Eventually(session).Should(Exit(0))
+					Expect(session).Should(Say("api endpoint:   %s", apiURL))
+				})
+			})
+
+			Describe("Insecure Endpoint", func() {
+				When("the API endpoint is insecure", func() {
+					It("shows a warning to the user", func() {
+						apiURL := helpers.GetAPI()
+						trimmedURL := strings.Trim(apiURL, "https://api")
+						insecureURL := strings.Replace(apiURL, "https:", "http:", -1)
+						session := helpers.CF("login", "-a", insecureURL)
+
+						Eventually(session).Should(Exit(1))
+						Expect(session).Should(Say("API endpoint: %s", insecureURL))
+						Expect(session).Should(Say("Warning: Insecure http API endpoint detected: secure https API endpoints are recommended"))
+						Expect(session).Should(Say("FAILED"))
+						Expect(session).Should(Say("Received invalid SSL certificate from login%s - unknown authority", trimmedURL))
+					})
+				})
+			})
+
+			Describe("SSL Validation", func() {
+				When("the ssl certificate is invalid", func() {
+					It("fails with an error message", func() {
+						apiURL := helpers.GetAPI()
+						trimmedURL := strings.Trim(apiURL, "https://")
+						session := helpers.CF("login", "-a", apiURL)
+						Eventually(session).Should(Exit(1))
+						Expect(session).Should(Say("API endpoint: %s", apiURL))
+						Expect(session).Should(Say("FAILED"))
+						Expect(session).Should(Say("Invalid SSL Cert for %s", trimmedURL))
+						Expect(session).Should(Say("TIP: Use 'cf login --skip-ssl-validation' to continue with an insecure API endpoint"))
+
+						session = helpers.CF("api")
+						Eventually(session).Should(Exit(0))
+						Expect(session).ShouldNot(Say("api endpoint:   %s", apiURL))
+					})
+				})
+				When("the ssl certificate is valid", func() {
+
+				})
+			})
 		})
 
-		It("prompts the user for an endpoint", func() {
-			session := helpers.CFWithStdin(buffer, "login")
-			Eventually(session).Should(Say("API endpoint>"))
-			session.Interrupt()
-			Eventually(session).Should(Exit())
+		When("the API endpoint is already set", func() {
+			It("does not promt the user for API endpoing", func() {
+				session := helpers.CF("login")
+				Consistently(session).ShouldNot(Say("API endpoint>"))
+				session.Interrupt()
+				Eventually(session).Should(Exit())
+			})
 		})
+
 	})
 
 	When("--sso-passcode flag is given", func() {
